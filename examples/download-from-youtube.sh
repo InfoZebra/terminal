ab8eb004053f9ac6fe76cf47389eec6424639c47
InfoZebra <79378855+InfoZebra@users.noreply.github.com>
2021-02-23 19:34:51 +0200

    Update download-from-youtube.sh

--- examples/download-from-youtube.sh
+++ examples/download-from-youtube.sh
@@ -1,1 +1,4 @@
-curl -d'{"method": "channel_create", "params": {"name": "@delete_channel", "bid": "0.001", "featured": [], "tags": ["Bitcoin", "Crypto", "Binance", "Trading", "BTC"], "languages": ["en"], "allow_duplicate_name": true, "locations": [], "funding_account_ids": [], "preview": false, "blocking": false}}' http://localhost:5279/
+# curl -d'{"method": "channel_create", "params": {"name": "@delete_channel", "bid": "0.001", "featured": [], "tags": ["Bitcoin", "Crypto", "Binance", "Trading", "BTC"], "languages": ["en"], "allow_duplicate_name": true, "locations": [], "funding_account_ids": [], "preview": false, "blocking": false}}' http://localhost:5279/
+
+read -p "Enter fullname: " fullname
+echo fullname
