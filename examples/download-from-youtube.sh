e12f174ae96bf2c1b389763b6f02b9bfeb3aa524
InfoZebra <79378855+InfoZebra@users.noreply.github.com>
2021-02-23 20:25:13 +0200

    Create download-from-youtube.sh

--- examples/download-from-youtube.sh
+++ examples/download-from-youtube.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-for arg in "$@"
-do
-index=$(echo $arg | cut -f1 -d=)
-val=$(echo $arg | cut -f2 -d=)
-case $index in
-X) x=$val;;
-Y) y=$val;;
-*)
-esac
-done
-((result=x+y))
-echo "X+Y=$result"
+curlout=$(curl "curl -d'{"method": "channel_create", "params": {"name": "@delete_channel", "bid": "0.001", "featured": [], "tags": ["Bitcoin", "Crypto", "Binance", "Trading", "BTC"], "languages": ["en"], "allow_duplicate_name": true, "locations": [], "funding_account_ids": [], "preview": false, "blocking": false}}' http://localhost:5279/");
+
+echo curlout | jq -r '.jsonrpc'
