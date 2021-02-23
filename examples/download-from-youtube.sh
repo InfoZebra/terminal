617defc24755c3ac1469624df68de40e5bb94c1b
InfoZebra <79378855+InfoZebra@users.noreply.github.com>
2021-02-23 19:31:27 +0200

    Update download-from-youtube.sh

--- examples/download-from-youtube.sh
+++ examples/download-from-youtube.sh
@@ -1,1 +1,1 @@
-mkdir delete
+curl -d'{"method": "channel_create", "params": {"name": "@delete_channel", "bid": "0.001", "featured": [], "tags": ["Bitcoin", "Crypto", "Binance", "Trading", "BTC"], "languages": ["en"], "allow_duplicate_name": true, "locations": [], "funding_account_ids": [], "preview": false, "blocking": false}}' http://localhost:5279/
